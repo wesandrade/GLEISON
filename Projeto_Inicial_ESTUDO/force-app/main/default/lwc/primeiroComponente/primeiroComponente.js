635916d66152b4cd7288e90cdbde6355d53a726a
wesandrade <80066306+wesandrade@users.noreply.github.com>
2026-05-16 05:09:59 -0300

    feat: Vitrine corrigida com ícones nativos e cabeçalho customizado

--- Projeto_Inicial_ESTUDO/force-app/main/default/lwc/primeiroComponente/primeiroComponente.js
+++ Projeto_Inicial_ESTUDO/force-app/main/default/lwc/primeiroComponente/primeiroComponente.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import LightningAlert from 'lightning/alert';
 
-export default class PrimeiroComponente extends LightningElement {}
+export default class PrimeiroComponente extends LightningElement {
+    
+    async handleSucesso(event) {
+        const itemId = event.detail.id;
+
+        // Alerta customizado de Item Criado
+        await LightningAlert.open({
+            message: `Item registrado com sucesso no banco de dados dos Irmãos ANDRADE! O produto foi vinculado ao pedido de destino. (ID do Registro: ${itemId})`,
+            theme: 'success',
+            label: '🚀 Item Adicionado!',
+        });
+    }
+}
